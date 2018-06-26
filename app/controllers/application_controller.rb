31e2d80559ad64da6203de94d1dfacbeab2cd0a4
Allegra <aukseallegra@gmail.com>
2018-06-26 22:59:30 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do   #loads new form
+    erb :new
   end
+
+  post '/posts' do     #creates a new blog post in "/posts"
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do       #loads all the blog entries
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do       #loads show page for the selected id
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do   #allows to edit the selected id
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do    #updates the post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do  #deletes the selected post
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 end
